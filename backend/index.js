2b0ec5c76970b044e9d1473f35dc19659b062cc3
Chethan-044 <chetuc0404@gmail.com>
2026-04-05 00:00:13 +0530

    implementing socket in index.js

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const connectDb = require("./config/dbconnect.js")
 const bodyparser =require("body-parser")
 const authRoute = require("./routes/authRoute.js")
 const chatRoute = require("./routes/chatRoute.js")
+const statusRoute = require("./routes/statusRoute.js")
+const initializeSocket = require("./services/socketService.js")
+const http = require("http") 
+
 dotenv.config();
 
 
@@ -13,20 +17,38 @@ dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
+const corsOption = {
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:['GET','POST','PUT','DELETE','OPTIONS'],
+}
+
 //middleware
+app.use(cors(corsOption))
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyparser.urlencoded({extended:true}))
 
-
 connectDb();
+
+//create http server
+const server = http.createServer(app);
+
+//initialize socket with the http server
+const io = initializeSocket(server);
+app.use((req,res,next)=>{
+    req.io = io; //attach the io instance to the request object for use in routes
+    req.socketUsermap = io.socketUsermap; //attach the online users map to the request object for use in routes
+    next();
+})
 
 
 //routes
 app.use("/api/auth",authRoute)
 app.use("/api/chat",chatRoute)
+app.use("/api/status",statusRoute)
 
-app.listen(PORT,(req,res)=>{
+server.listen(PORT,(req,res)=>{
     console.log("App running at port",PORT);
     
 })
